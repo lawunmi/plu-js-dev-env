2693d5f03a2d42b17a8f3abef1286c77e5c632bc
lawunmi <olawunmiraheem@gmail.com>
2019-07-26 09:20:01 +0100

    Update new file created

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 import './index.css';
-import numeral from 'numeral';
+import {getUsers, deleteUser} from './api/UserApi'
+
+//populate table of users via API call.
+getUsers().then(result => {
+    let usersBody = ""
+
+    result.forEach(user => {
+        usersBody+= `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstname}</td>
+        <td>${user.lastname}</td>
+        <td>${user.email}</td>
+        </tr>`
+    });
+
+        global.ducument.getElementById('users').innerHTML = usersBody;
+
+        const deleteLinks = global.decodeURIComponent.getElementsByClassName('deleteUser');
+
+        // Must use array.from to create a real array from a DOM collection 
+        // getElementByClassName only returns an "array like" object
+        Array.from(deleteLinks, link => {
+            link.onclick = function(event) {
+                const element = event.target;
+                event.preventDefault();
+                deleteUser(element.attributes["data-id"].value);
+                const row = element.parentNode.parentNode;
+                row.parentNode.removeChild(row);
+            };
+
+        });
+});
+
+
+/*import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0.00');
-console.log(`I would pay ${courseValue} for this awesome course!`);
+console.log(`I would pay ${courseValue} for this awesome course!`);*/
